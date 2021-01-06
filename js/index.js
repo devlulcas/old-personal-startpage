139bed36d5d0ad5ef61005c2c1bf6eeeba35ee7f
devlulcas <lucasalvesrego.contato@gmail.com>
2021-01-06 20:23:22 -0300

    now the buttom color change if the search input is in input event

--- js/index.js
+++ js/index.js
@@ -1,24 +1,31 @@
 const hour = document.getElementById('hour')
+const searchInput = document.getElementById('search_input')
+const searchButton = document.getElementById('search_btn')
 
-
-
-function atualizeTime() {
+function atualizeTime () {
     const date = new Date
     let h = date.getHours()
     let m = date.getMinutes()
     let s = date.getSeconds()
-    if (h<10){
+    if (h < 10) {
         h = '0' + h
     }
-    if (m<10){
+    if (m < 10) {
         m = '0' + m
     }
-    if (s<10){
+    if (s < 10) {
         s = '0' + s
     }
     hour.textContent = `${h}:${m}:${s}`
-    console.log("a")
-
     setInterval(atualizeTime, 500)
 }
 atualizeTime()
+
+searchInput.addEventListener('input', (e)=>{
+    if(e.target.value != ''){
+        searchButton.style.backgroundColor = `${getComputedStyle(searchButton).getPropertyValue('--light-color')}`
+    }
+    else{
+        searchButton.style.backgroundColor = `${getComputedStyle(searchButton).getPropertyValue('--item-color')}`
+    }
+})
